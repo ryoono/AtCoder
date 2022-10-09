d70560f8daecf7f3a22d26bd88f45fce452ae128
k16025kk <ono@pluslab.org>
2022-10-09 11:38:20 +0900

    Dやりかけ

--- ABC272/D/main.cpp
+++ ABC272/D/main.cpp
@@ -23,9 +23,20 @@ int main( void ){
 
     for( ui i=1; i<=buf; ++i ){
         for( ui j=i; j<=buf; ++j ){
-            
+            // 移動先リストを作成
         }
     }
+
+    si fi[n][n];
+    for( ui i=0; i<n; ++i ){
+        for( ui j=0; j<n; ++j ){
+            fi[i][j] = -1;
+        }
+    }
+    fi[0][0] = 0;
+    si cnt = 0;
+
+    // 移動先リストをもとにcnt回目の動作を実施する
 
 
     return 0;
